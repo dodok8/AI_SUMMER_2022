33d3c74f741e3fa91279b5e35f2a8637b2f663b3
Kim, Hyeonseo <dodok8@gmail.com>
2022-07-15 17:46:48 +0900

    Update youtube_face_landmark.py

--- face_landmark/youtube_face_landmark.py
+++ face_landmark/youtube_face_landmark.py
@@ -5,13 +5,12 @@ import dlib
 import cv2
 import time
 import os
+import pathlib
 import argparse
 
 from landmark_utils import show_raw_landmarks, show_landmark_shape
-import sys
-sys.path.insert(0, '../utils')
-from youtube_video import YoutubeStream
 
+import yt_dlp
 
 if __name__ == "__main__":
     # 인자로 데이터의 경로를 받습니다
@@ -24,19 +23,36 @@ if __name__ == "__main__":
     detector = dlib.get_frontal_face_detector()
     predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
     
-    # 유튜브 동영상을 생성합니다
-    video = YoutubeStream(args.youtube_url)
+    # 유튜브 동영상을 다운로드합니다
 
-    print("이미지 창에서 esc 버튼을 눌러 종료하세요. 얼굴 특징점 추출을 시작합니다")
-    for ret, frame in video.get_stream():
-        if not ret:
-            print("비디오를 가져오는 중 오류가 발생했습니다")
-            break
+    ydl_opts = {
+        "format": "mp4/bestaudio/best",
+    }
 
-        image = imutils.resize(frame, width=500)
-        show_landmark_shape(image, detector, predictor) # 동영상 내 사람 얼굴을 탐지하고 보여줍니다
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info=ydl.extract_info(args.youtube_url, download=False)
+        ydl.download([info['webpage_url']])
+        file_name=ydl.prepare_filename(info)
+        new_path = pathlib.Path().absolute().joinpath(file_name)
 
-        if cv2.waitKey(1) > 0:
-            break
+        if not pathlib.Path(new_path).is_file():
+            print("파일 다운로드에 실패하였습니다.")
+        else: 
+            print("이미지 창에서 esc 버튼을 눌러 종료하세요. 얼굴 특징점 추출을 시작합니다")
 
-    print("얼굴 특징점 추출을 종료합니다")
+            cap = cv2.VideoCapture(str(new_path))
+            
+            while True:
+                ret, frame = cap.read()
+                if not ret:
+                    print("비디오를 가져오는 중 오류가 발생했습니다")
+                    break
+
+                image = imutils.resize(frame, width=500)
+                show_landmark_shape(image, detector, predictor) # 동영상 내 사람 얼굴을 탐지하고 보여줍니다
+
+                if cv2.waitKey(1) > 0:
+                    break
+
+            print("얼굴 특징점 추출을 종료합니다")
+
